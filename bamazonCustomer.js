013041f79aef3fb0b7c7027ab638277474418df5
Wynn Blevins <wynnblevins@yahoo.com>
2018-04-13 14:13:05 -0400

    Products are now being displayed in a formatted table.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const db = require('mysql');
+const Table = require('cli-table');
 
 var connection = db.createConnection({
     host     : process.env.DB_HOST,
@@ -11,7 +12,31 @@ connection.connect();
 
 connection.query('SELECT * FROM products;', 
     function (error, results, fields) {;
-    console.log(results);
+    
+    if (!error) {
+        displayTable(results);
+    } else {
+        var errorMsg = `Oppsie woopsie, something went wrong grabbing data \n${error}`;
+        throw errorMsg;
+    }    
 });
+
+function displayTable(results) {
+    var table = new Table({ head: [
+        "ID", "Product Name", "Department Name", "Price", "Quantity"] 
+    });
+    
+    for (var i = 0; i < results.length; i++) {
+        table.push([
+            results[i].item_id, 
+            results[i].product_name,
+            results[i].department_name,
+            results[i].price,
+            results[i].stock_quantity            
+        ]);
+    }
+
+    console.log(table.toString());
+}
 
 connection.end();
